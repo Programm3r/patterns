40f7eb1b55c82154f0df1c8a3ae711ccb3d26466
rao4 <rohara@raymourflanigan.com>
2017-10-06 09:54:16 -0400

    Adapter Pattern Console app

---
+++ Patterns/ConsoleApplication1/IEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public interface IEnemy
+    {
+        void doDamage();
+        void moveForward();
+        void interactWith(string Name);
+    }
+}
---
+++ Patterns/ConsoleApplication1/RobotEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class RobotEnemy
+    {
+
+        private Random generator = new Random();
+
+        public void HitOpponent()
+        {
+            int damage = generator.Next(10) + 1;
+
+            Console.WriteLine("Enemy robot causes " + damage.ToString() + " to opponent.");
+        }
+
+        public void walkForward()
+        {
+            int walk = generator.Next(10) + 1;
+
+            Console.WriteLine("Enemy robot walks " + walk.ToString() + " spaces.");
+        }
+
+        public void ReactToHuman(string Name)
+        {
+            Console.WriteLine("Robot Enemy Tromples " + Name);
+        }
+
+    }
+}
