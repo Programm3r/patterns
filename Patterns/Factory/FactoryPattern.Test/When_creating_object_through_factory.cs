6778088367b2e360bcca4f3dfe7ce59fad8e3130
Wojciech Dąbrowski <wojtasop@gmail.com>
2018-10-27 17:22:24 +0200

    Added meaningful tests for factory pattern.

---
+++ Patterns/Factory/FactoryPattern.Test/When_creating_object_through_factory.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace FactoryPattern.Test
+{
+    [TestFixture]
+    public class When_creating_object_through_factory
+    {
+        [Test]
+        public void It_should_create_correct_instance_of_honda()
+        {
+            // Given
+            var carFactory = new Factory<Car>();
+
+            // When
+            IProduct<Car> carProduct = carFactory.Create<Honda>();
+
+            // Then
+            Assert.IsInstanceOf<Honda>(carProduct);
+        }
+
+        [Test]
+        public void It_should_create_correct_instance_of_toyota()
+        {
+            // Given
+            var carFactory = new Factory<Car>();
+
+            // When
+            IProduct<Car> carProduct = carFactory.Create<Toyota>();
+
+            // Then
+            Assert.IsInstanceOf<Toyota>(carProduct);
+        }
+    }
+}
