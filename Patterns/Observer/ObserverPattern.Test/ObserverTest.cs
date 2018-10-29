a39b52174a1ab675c59a0e7ab3314a4fd12df8a6
Unknown <e185150@metu.edu.tr>
2018-10-29 15:09:22 +0300

    Observer pattern and its tests are added.

---
+++ Patterns/Observer/ObserverPattern.Test/ObserverTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using ObserverPattern;
+
+namespace ObserverPattern.Test
+{
+    [TestFixture]
+    public class ObserverTest
+    {
+        [TestCase]
+        public void Test1(int killCount)
+        {
+            Achievement achievement1Observer = new Achievement();
+            Achievement achievement2Observer = new Achievement();
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            player1.Register(achievement1Observer);
+            player2.Register(achievement2Observer);
+
+            string achivementKey = "Achievement-" + "5 Spiders Killed";
+
+            for (int i = 0; i < 5; i++)
+            {
+                player1.KillSpider();
+            }
+
+            for(int i = 0; i < 3; i++)
+            {
+                player2.KillSpider();
+            }
+
+            Assert.IsTrue(achievement1Observer.achievementList.Contains(achivementKey));
+            Assert.IsFalse(achievement2Observer.achievementList.Contains(achivementKey));
+        }
+    }
+}
---
+++ Patterns/Observer/ObserverPattern.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ObserverPattern.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ObserverPattern.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d17af32-3418-4ca3-acdd-aa814e798c9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
